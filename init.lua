2b91a58c39430fdf8651c1a8164185664b1fcab6
Per Johansson <per.johansson.1@svt.se>
2024-01-29 21:28:33 +0100

    change to rustacean

--- init.lua
+++ init.lua
@@ -1,2 +1,2 @@
--- bootstrap lazy.nvim,  u LazyVim and your plugins
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -12,33 +12,14 @@ return {
       require("Comment").setup()
     end,
   },
-  --customize telescope
-  -- which-key extensions
   {
-    "pocco81/auto-save.nvim",
-    config = function(_, opts)
-      require("auto-save").setup({
-        enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
-        execution_message = {
-          message = nil,
-        },
-        -- function that determines whether to save the current buffer or not
-        -- return true: if buffer is ok to be saved
-        -- return false: if it's not ok to be saved
-        condition = function(buf)
-          local fn = vim.fn
-          local utils = require("auto-save.utils.data")
-
-          if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
-            return true -- met condition(s), can save
-          end
-          return false -- can't save
-        end,
-        write_all_buffers = false, -- write all buffers when the current one meets `condition`
-        debounce_delay = 135, -- saves the file at most every `debounce_delay` milliseconds
-      })
-    end,
+    "okuuva/auto-save.nvim",
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = {
+      execution_message = {
+        enabled = false,
+      },
+    },
   },
-  -- git blame
-  -- add symbols-outline
 }
